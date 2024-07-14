8eed7dd32c30b59028507920921c4d8df5f1725b
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2024-07-14 19:17:14 +0500

    bug

--- my_util_functions/profile_util_functions.py
+++ my_util_functions/profile_util_functions.py
@@ -107,9 +107,7 @@ def today_user_score(user_id, db):
 
 
 def user_salaries(user_id, db, date=None, this_month=None, start_date=None, end_date= None):
-    start_datetime = datetime.datetime.combine(start_date, time.min)
-    # Convert end_date to a datetime at the end of the day
-    end_datetime = datetime.datetime.combine(end_date, time.max)
+    
     if date:
         user_salaries = db.query(models.UserSalaries)\
             .filter(models.UserSalaries.receiver_id == user_id)\
@@ -127,6 +125,9 @@ def user_salaries(user_id, db, date=None, this_month=None, start_date=None, end_
                                 )).all()
     
     elif start_date and end_date:
+        start_datetime = datetime.datetime.combine(start_date, time.min)
+        # Convert end_date to a datetime at the end of the day
+        end_datetime = datetime.datetime.combine(end_date, time.max)
         user_salaries = db.query(models.UserSalaries)\
             .filter(models.UserSalaries.receiver_id == user_id)\
                 .filter(\
